d08b6cd560b8e5a94683c54635f33b8d7cd8538a
LovisaBrorson <lovisabrorson@gmail.com>
2022-06-09 11:26:21 +0200

    Added css and img to Contact

--- front-end/src/Components/contact.js
+++ front-end/src/Components/contact.js
@@ -3,9 +3,16 @@ import React from "react"
 export const Contact = () => {
   return (
     <article>
-      <h1>Kontakta Börje</h1>
-      <p>Telefon: 0322-661064 Mobil: 0729-380345 Mail: broakern@gmail.com</p>
-      <p>infoga instagramloggan</p>
+      <div className='wrapper'>
+        <div className='phone'></div>
+        <div className='instagram-logo'></div>
+        <div className='contact-text'>
+          <h1>Kontakta Börje</h1>
+          <p>Telefon: 0322-661064 </p>
+          <p>Mobil: 0729-380345 </p>
+          <p>Mail: broakern@gmail.com</p>
+        </div>
+      </div>
     </article>
   )
 }
