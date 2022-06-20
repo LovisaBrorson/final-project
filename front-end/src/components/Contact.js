c5c5f162abe9deb3a59ed9575abcca598ff0d4ba
LovisaBrorson <lovisabrorson@gmail.com>
2022-06-20 14:58:08 +0200

    worked with the instagram logo on contactpage

--- front-end/src/components/Contact.js
+++ front-end/src/components/Contact.js
@@ -1,6 +1,6 @@
 import React from "react"
 import phone from "../assets/telefonlur-2.jpg"
-// import instagram from "../assets/instagram-logga.png"
+import instagram from "../assets/instagram-logga.png"
 
 export const Contact = () => {
   return (
@@ -8,16 +8,13 @@ export const Contact = () => {
       <div className='wrapper-contact'>
         <img src={phone} alt='Phone' className='phone' />
 
-        {/* <div className='text-and-instagram-wrapper'> */}
-        {/* <img src={instagram} alt='Instagram' className='instagram' /> */}
-
         <div className='contact-text'>
           <h1>KONTAKTA BÖRJE</h1>
           <p>Telefon: 0322-661064 </p>
           <p>Mobil: 0729-380345 </p>
           <p>Mail: broakern@gmail.com</p>
+          <img src={instagram} alt='Instagram' className='instagram' />
         </div>
-        {/* </div> */}
       </div>
     </article>
   )
