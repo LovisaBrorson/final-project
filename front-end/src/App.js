887ec0b9bd4f07503288047375e387eeef00f03c
LovisaBrorson <lovisabrorson@gmail.com>
2022-06-21 13:46:21 +0200

    Added cookiebanner in the bottom

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
+import CookieConsent from "react-cookie-consent"
 
 import "./guestbook.css"
 
@@ -14,27 +15,47 @@ import NotFond from "./components/Notfond"
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
+    <>
+      <BrowserRouter>
+        <Navbar />
 
-      <Routes>
-        <Route path='/' element={<Welcome />} />
+        <Routes>
+          <Route path='/' element={<Welcome />} />
 
-        <Route path='/contact' element={<Contact />} />
+          <Route path='/contact' element={<Contact />} />
 
-        <Route path='/info' element={<Info />} />
+          <Route path='/info' element={<Info />} />
 
-        <Route path='/gallery' element={<Gallery />} />
+          <Route path='/gallery' element={<Gallery />} />
 
-        <Route path='/exhibit' element={<Exhibit />} />
+          <Route path='/exhibit' element={<Exhibit />} />
 
-        <Route path='/guestbook' element={<Guestbookapp />} />
+          <Route path='/guestbook' element={<Guestbookapp />} />
 
-        <Route path='/404' element={<NotFond />} />
+          <Route path='/404' element={<NotFond />} />
 
-        <Route path='*' element={<Navigate to='/404' replace />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path='*' element={<Navigate to='/404' replace />} />
+        </Routes>
+      </BrowserRouter>
+      <CookieConsent
+        debug={true}
+        location='bottom'
+        style={{
+          background: "#b4cfb0",
+          color: " #66806a",
+          fontSize: "20px",
+        }}
+        buttonStyle={{
+          background: "#66806a",
+          color: "#fff",
+          fontSize: "20px",
+          padding: "15px",
+        }}
+        buttonText='Jag gillar kakor!'
+      >
+        Den här sidan använder små goda kakor 🍪
+      </CookieConsent>
+    </>
   )
 }
 
