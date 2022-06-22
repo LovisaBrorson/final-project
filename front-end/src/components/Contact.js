75797383e5e655615c22201b2ff4dcea56d23dff
LovisaBrorson <lovisabrorson@gmail.com>
2022-06-22 10:35:01 +0200

    added link to email

--- front-end/src/components/Contact.js
+++ front-end/src/components/Contact.js
@@ -11,14 +11,12 @@ export const Contact = () => {
         <div className='contact-text'>
           <h1>KONTAKTA BÖRJE</h1>
           <p>Mobil: 0729-380345 </p>
-          <a href='https://google.com'>
-            <p>Telefon: 0322-661064 </p>{" "}
-          </a>
-
-          <a className='email' href='mailto:broakern@gmail.com'>
-            broakern@gmail.com
-          </a>
-          <p>Mail:</p>
+          <p>Telefon: 0322-661064 </p>
+          <p>
+            <a className='email' href='mailto:broakern@gmail.com'>
+              Mail: broakern@gmail.com
+            </a>
+          </p>
           <div className='wrapper-instagram'>
             <img src={instagram} alt='Instagram-logo' className='instagram' />
             <spam>Borje_Brorson_akvarell</spam>
