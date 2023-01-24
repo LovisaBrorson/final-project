c24a15a1b0324521a7f0742f78624d75f5569c16
LovisaBrorson <lovisabrorson@gmail.com>
2023-01-24 22:39:53 +0100

    Testing

--- front-end/src/utils/urls.js
+++ front-end/src/utils/urls.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://finalproject-ahgn2jjgba-lz.a.run.app/greetings"
+export const API_URL = "https://finalproject-ahgn2jjgba-lz.a.run.app/"
 
 /*export const API_URL = "https://borjebrorson.herokuapp.com/greetings" */
